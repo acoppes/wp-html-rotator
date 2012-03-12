e4255f9e87441321595f9bdbbe9ae554a463dde4
Ariel Coppes <ariel.coppes@gemserk.com>
2012-03-12 19:09:37 -0300

    initial commit, adding license.txt, readme.txt, etc

---
+++ wp-html-rotator.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: WP HTML Rotator
+Plugin URI: 
+Description: Rotate HTML sections based on the timezone and hour of day.
+Version: 0.0.1-SNAPSHOT
+Author: Ariel Coppes
+Author URI: 
+*/
+
+function html_rotation_section($atts, $content = null) {
+
+}
+
+add_shortcode('html_rotation_section', 'html_rotation_section');
+
+?>
