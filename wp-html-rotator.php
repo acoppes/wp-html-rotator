63da2633d595e836b4abd86f9a17e0dae8010d56
ariel.coppes <ariel.coppes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-16 19:20:09 +0000

    started to make some tests using dates to show or not the content of the shortcodes
    git-svn-id: http://plugins.svn.wordpress.org/wp-html-rotator/trunk@519977 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-html-rotator.php
+++ wp-html-rotator.php
@@ -11,18 +11,59 @@ Author URI:
 
 function html_rotation_section($atts, $content = null) {
 
+	$DEBUG = TRUE;
+
 	// defines the default function parameter values
 	$default_atts = array( 
 		'visible' => "true", 
+		'start' => null,
+		'end' => null,
 	);
 
 	// merges the default parameters with the current parameters
 	$values = shortcode_atts($default_atts, $atts);
 
 	$visible = $values['visible'];
+	$start = $values['start'];
+	$end = $values['end'];
 
 	if ($visible != "true")
 		return null;
+
+	if ($start == null)
+		return "start attribute not provided";
+
+	if ($end == null)
+		return "end attribute not provided";
+
+	$start_time = strtotime($start);
+	$end_time = strtotime($end);
+
+	if ($start_time == -1 or $start_time == FALSE) {
+		if ($DEBUG)
+			return "Wrong start attribute";
+		return null;
+	}
+
+	if ($end_time == -1 or $end_time == FALSE) {
+		if ($DEBUG == TRUE)
+			return "Wrong end attribute";
+		return null;
+	}
+
+	$current_time = time();
+
+	if ($DEBUG) {
+		echo "current_time: ".date('D, d M Y H:i:s', $current_time)."<br/>";
+		echo "start_time: ".date('D, d M Y H:i:s', $start_time)."<br/>";
+		echo "end_time: ".date('D, d M Y H:i:s', $end_time)."<br/>";
+	}
+
+	if ($current_time < $start_time)
+		return "current time: ".date('D, d M Y H:i:s', $current_time)." < start time: ".date('D, d M Y H:i:s', $start_time);
+
+	if ($current_time > $end_time)
+		return "current time > end time";
 
 	return $content;
 }
